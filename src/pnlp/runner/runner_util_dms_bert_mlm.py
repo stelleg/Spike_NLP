8015009a78b70ad68f7a4f6ce1008e13d0e76aa3
Kaetlyn Gibson <kaetlynrg@gmail.com>
2024-12-19 18:53:18 -0700

    update some model comparisons

--- src/pnlp/runner/runner_util_dms_bert_mlm.py
+++ src/pnlp/runner/runner_util_dms_bert_mlm.py
@@ -125,39 +125,6 @@ def load_model_checkpoint(path_to_pth, metrics_csv, starting_epoch):
                 break
             fb.write(line)
 
-# def plot_log_file(metrics_csv, metrics_img, rmse_model):
-#     df = pd.read_csv(metrics_csv)
-
-#     sns.set_theme(style="darkgrid")
-#     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(16, 18))  # 2 rows, 1 column
-#     fontsize = 28
-
-#     # Plot Loss
-#     ax1.plot(df['Epoch'], df['Test MLM Loss'], label='Test MLM Loss', color='tab:orange', linestyle='dashed', linewidth=3)
-#     ax1.plot(df['Epoch'], df['Train MLM Loss'], label='Train MLM Loss', color='tab:red', linestyle='dashed', linewidth=3)
-#     ax1.plot(df['Epoch'], df[f'Test {rmse_model} RMSE'], label=f'Test {rmse_model} RMSE Loss', color='tab:orange', linestyle='dashdot', linewidth=3)
-#     ax1.plot(df['Epoch'], df[f'Train {rmse_model} RMSE'], label=f'Train {rmse_model} RMSE Loss', color='tab:red', linestyle='dashdot', linewidth=3)
-#     ax1.plot(df['Epoch'], df['Test Loss'], label='Test Loss', color='tab:orange', linewidth=3)
-#     ax1.plot(df['Epoch'], df['Train Loss'], label='Train Loss', color='tab:red', linewidth=3)
-#     ax1.tick_params(axis='x', labelsize=fontsize)
-#     ax1.set_ylabel('Loss', fontsize=fontsize)
-#     ax1.tick_params(axis='y', labelsize=fontsize)
-#     ax1.legend(loc='upper right', fontsize=fontsize)
-
-#     # Plot Accuracy
-#     ax2.plot(df['Epoch'], df['Test MLM Accuracy'], label='Test MLM Accuracy', color='tab:green', linewidth=3)
-#     ax2.plot(df['Epoch'], df['Train MLM Accuracy'], label='Train MLM Accuracy', color='tab:blue', linewidth=3)
-#     ax2.set_xlabel('Epochs', fontsize=fontsize)
-#     ax2.tick_params(axis='x', labelsize=fontsize)
-#     ax2.set_ylabel('Accuracy', fontsize=fontsize)
-#     ax2.tick_params(axis='y', labelsize=fontsize)
-#     ax2.set_ylim(0, 100) 
-#     ax2.legend(loc='lower right', fontsize=fontsize)
-
-#     plt.tight_layout()
-#     plt.savefig(metrics_img, format='pdf')
-#     plt.savefig(metrics_img.replace('.pdf', '.png'), format='png')
-
 def plot_log_file(metrics_csv, metrics_img, rmse_model):
     df = pd.read_csv(metrics_csv)
 
@@ -179,7 +146,7 @@ def plot_log_file(metrics_csv, metrics_img, rmse_model):
     plt.savefig(metrics_img, format='pdf')
     plt.savefig(metrics_img.replace('.pdf', '.png'), format='png')
 
-def plot_log_file_BE(metrics_csv, metrics_img, rmse_model):
+def plot_log_file_BE(metrics_csv, metrics_img):
     df = pd.read_csv(metrics_csv)
 
     # Plotting
@@ -188,9 +155,9 @@ def plot_log_file_BE(metrics_csv, metrics_img, rmse_model):
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(16, 18))  # 2 rows, 1 column
 
     # Plot 1: Training Metrics (binding, expression, combined RMSE)
-    ax1.plot(df['Epoch'], df[f'Train {rmse_model} Binding RMSE'], label=f'Train {rmse_model} Binding RMSE', color='tab:blue', linewidth=3)
-    ax1.plot(df['Epoch'], df[f'Train {rmse_model} Expression RMSE'], label=f'Train {rmse_model} Expression RMSE', color='tab:green', linewidth=3)
-    ax1.plot(df['Epoch'], df[f'Train {rmse_model} RMSE'], label=f'Train {rmse_model} RMSE (Combined)', color='tab:orange', linewidth=3)
+    ax1.plot(df['Epoch'], df['Train Binding RMSE'], label='Train Binding RMSE', color='tab:blue', linewidth=3)
+    ax1.plot(df['Epoch'], df['Train Expression RMSE'], label='Train Expression RMSE', color='tab:green', linewidth=3)
+    ax1.plot(df['Epoch'], df['Train RMSE'], label='Train RMSE (Combined)', color='tab:orange', linewidth=3)
 
     ax1.tick_params(axis='x', labelsize=fontsize)
     ax1.set_ylabel('RMSE', fontsize=fontsize)
@@ -199,9 +166,9 @@ def plot_log_file_BE(metrics_csv, metrics_img, rmse_model):
     ax1.set_title('Training Metrics', fontsize=fontsize)
 
     # Plot 2: Testing Metrics (binding, expression, combined RMSE)
-    ax2.plot(df['Epoch'], df[f'Test {rmse_model} Binding RMSE'], label=f'Test {rmse_model} Binding RMSE', color='tab:blue', linewidth=3)
-    ax2.plot(df['Epoch'], df[f'Test {rmse_model} Expression RMSE'], label=f'Test {rmse_model} Expression RMSE', color='tab:green', linewidth=3)
-    ax2.plot(df['Epoch'], df[f'Test {rmse_model} RMSE'], label=f'Test {rmse_model} RMSE (Combined)', color='tab:orange', linewidth=3)
+    ax2.plot(df['Epoch'], df['Test Binding RMSE'], label='Test Binding RMSE', color='tab:blue', linewidth=3)
+    ax2.plot(df['Epoch'], df['Test Expression RMSE'], label='Test Expression RMSE', color='tab:green', linewidth=3)
+    ax2.plot(df['Epoch'], df['Test RMSE'], label='Test RMSE (Combined)', color='tab:orange', linewidth=3)
 
     ax2.set_xlabel('Epochs', fontsize=fontsize)
     ax2.tick_params(axis='x', labelsize=fontsize)
